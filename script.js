8cbeffced15396431cfb8e76cc2008086af1081e
shyaboi <ian.shane.sears@gmail.com>
2020-02-05 18:36:48 -0700

    0.01 added more password choice params, and established params rando functions

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// DOM maniupulatation elements
+const passwordEl = document.getElementById('password')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numberEl = document.getElementById('number')
+const symbolEl = document.getElementById('symbol')
+const generateEl = document.getElementById('generate')
+//Rando character constants
+const randoChar = {
+  lower: randoLower,
+  upper: randoUpper,
+  number: randoNum,
+  symbol: randoASCII
+};
+// random lowercase letter function
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function randoLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// random uppercase letter function
+function randoUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+// random number function
+function randoNum() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+// random symbol function
+function randoASCII() {
+  const symbols = '!@#$%^&*(){}|\+=_-<>?/"';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
